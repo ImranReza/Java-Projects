005fc8cc354fe0395d1f3d7b57751d51638ceece
ImranReza <ImranReza.cse@gmail.com>
2024-07-31 13:16:42 +0600

    it a simple java project that calculate Mass and Weight

---
+++ MassAndWeightCalculator/src/main/java/com/mycompany/massandweightcalculator/MassAndWeightCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.massandweightcalculator;
+
+/**
+ *
+ * @author IMRAN
+ */
+public class MassAndWeightCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
